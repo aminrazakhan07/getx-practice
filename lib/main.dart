11ac9d150b8e2475ad3500ecf2beec77f93facb4
Raza <razaamin07@gmail.com>
2025-01-24 20:38:42 +0500

    img inimation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:getx_prectise/Medical%20App/Onboadring/OnboardHome.dart';
+import 'package:getx_prectise/Medical%20App/Screens/HomeScr.dart';
 import 'package:getx_prectise/PageBinding/page1/HomeView.dart';
 import 'package:getx_prectise/PageBinding/page1/binding.dart';
 import 'package:getx_prectise/PageBinding/page2/HomeView.dart';
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       debugShowCheckedModeBanner: false,
-      home: Onboardring(),
+      home: HomescrMedi(),
     );
   }
 }
